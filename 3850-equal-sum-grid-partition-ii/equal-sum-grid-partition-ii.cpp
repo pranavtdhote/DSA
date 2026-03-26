00d3c9c825bb561e5ca2914002d9bcb76d4de4c8
Pranav Dhote <166908644+pranavtdhote@users.noreply.github.com>
2026-03-26 23:03:01 +0530

    Time: 804 ms (69.23%) | Memory: 378.2 MB (64.61%) - LeetSync

--- 3850-equal-sum-grid-partition-ii/equal-sum-grid-partition-ii.cpp
+++ 3850-equal-sum-grid-partition-ii/equal-sum-grid-partition-ii.cpp
@@ -1,59 +1,67 @@
 class Solution {
 public:
-    typedef long long ll;
-    bool solve(vector<vector<int>>&grid){
-        int n=grid.size(),m=grid[0].size();
-        ll bottomSum=0,topSum=0;
-        vector<int>bottomFreq(100001,0),topFreq(100001,0);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                bottomSum+=grid[i][j];
-                bottomFreq[grid[i][j]]++;
+    vector<vector<int>> rotation(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector tmp(n, vector<int>(m));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                tmp[j][m - 1 - i] = grid[i][j];
             }
         }
-        for(int i=0;i<n-1;i++){
-           for(int j=0;j<m;j++){
-                bottomSum-=grid[i][j];
-                bottomFreq[grid[i][j]]--;
-                topSum+=grid[i][j];
-                topFreq[grid[i][j]]++;
-           }
-           if(topSum==bottomSum)return true;
-           ll diffTop=topSum-bottomSum;
-           if(diffTop>0 && diffTop<=100000){
-              int h=i+1,w=m;
-              if(h>1 && w>1){
-                if(topFreq[diffTop])return true;
-                }
-                else if(h>1 && w==1){
-                    if(grid[0][0]==diffTop || grid[i][0]==diffTop)return true;
-                }else if(h==1 && w>1){
-                    if(grid[0][0]==diffTop || grid[0][w-1]==diffTop)return true;
-                }
-           }
-           ll diffBot=bottomSum-topSum;
-           if(diffBot>0 && diffBot<=100000){
-            int h=(n-i-1),w=m;
-            if(h>1 && w>1){
-                if(bottomFreq[diffBot])return true;
-            }else if(h>1 && w==1){
-                if(grid[i+1][0]==diffBot || grid[n-1][0]==diffBot)return true;
-            }else if(h==1 && w>1){
-                if(grid[n-1][0]==diffBot || grid[n-1][w-1]==diffBot)return true;
-            }
-           }
-        }
-        return false;
+        return tmp;
     }
     bool canPartitionGrid(vector<vector<int>>& grid) {
-        if(solve(grid))return true;
-        int n=grid.size(),m=grid[0].size();
-        vector<vector<int>>mat(m,vector<int>(n));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                mat[j][i]=grid[i][j];
+        long long total = 0;
+        long long sum;
+        long long tag;
+        int m = grid.size();
+        int n = grid[0].size();
+        unordered_set<long long> exist;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                total += grid[i][j];
             }
         }
-        return solve(mat);
+        for (int k = 0; k < 4; k++) {
+            exist.clear();
+            exist.insert(0);
+            sum = 0;
+            m = grid.size();
+            n = grid[0].size();
+            if (m < 2) {
+                grid = rotation(grid);
+                continue;
+            }
+            if (n == 1) {
+                for (int i = 0; i < m - 1; i++) {
+                    sum += grid[i][0];
+                    tag = sum * 2 - total;
+                    if (tag == 0 || tag == grid[0][0] || tag == grid[i][0]) {
+                        return true;
+                    }
+                }
+                grid = rotation(grid);
+                continue;
+            }
+            for (int i = 0; i < m - 1; i++) {
+                for (int j = 0; j < n; j++) {
+                    exist.insert(grid[i][j]);
+                    sum += grid[i][j];
+                }
+                tag = sum * 2 - total;
+                if (i == 0) {
+                    if (tag == 0 || tag == grid[0][0] ||
+                        tag == grid[0][n - 1]) {
+                        return true;
+                    }
+                    continue;
+                }
+                if (exist.contains(tag)) {
+                    return true;
+                }
+            }
+            grid = rotation(grid);
+        }
+        return false;
     }
 };
